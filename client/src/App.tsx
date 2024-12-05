899f3052b90db6cb3e2017d8f96a32beeaa6f176
Utkarsh <utkarshjaiswal537@gmail.com>
2024-12-06 02:15:22 +0530

    Added Dashboard Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Landing from "./pages/Landing";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import ProfilePage from "./pages/ProfilePage";
+import ExpertDashboard from "./pages/ExpertDashboard";
 
 function App() {
   const handleLogin = (email : string, token : string) => {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Landing/>}/>
         <Route path="/login" element={<LoginPage onLogin={handleLogin}/>}/>
         <Route path="/signup" element={<SignupPage/>}/>
+        <Route path="/dashboard" element={<ExpertDashboard/>}/>
         <Route path="/profile" element={<ProfilePage/>}/>
       </Routes>
     </Router>
