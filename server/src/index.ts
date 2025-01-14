dc6444fa0a1c0d4d943f173438c161c8c488b1a6
Utkarsh <utkarshjaiswal537@gmail.com>
2025-01-14 23:06:01 +0530

    increased payload size of express json

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,8 +7,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
 app.get("/", (req, res) => {
   res.json({
