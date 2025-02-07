ce78f7eeba15cb997dfa8e738abf061675e0f7db
Utkarsh <utkarshjaiswal537@gmail.com>
2025-02-07 23:01:45 +0530

    minor fix

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -72,7 +72,7 @@ export const signupMentor = async (req: any, res: any) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true, // make this true when using for production
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -149,7 +149,7 @@ export const signupMentee = async (req: any, res: any) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true, // make this true when using for production
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -209,7 +209,7 @@ export const login = async (req: any, res: any) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,     // will be true in production
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
