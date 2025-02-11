0b1aa958094d287ac651c8a363f6cc38143a163f
Utkarsh <utkarshjaiswal537@gmail.com>
2025-02-11 19:42:43 +0530

    fixx

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -14,6 +14,7 @@ const setCookieOptions = {
   httpOnly: true,
   secure: true,       // must: make this true when using for production
   maxAge: 7 * 24 * 60 * 60 * 1000,
+  domain: 'mentg.in',
 }
 
 export const signupMentor = async (req: any, res: any) => {
