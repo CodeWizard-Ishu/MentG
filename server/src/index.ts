dc63d10c6efaa9663a93f2a86a87855db84a96fa
Utkarsh Jaiswal <89697358+CodeWizard-Ishu@users.noreply.github.com>
2025-04-27 14:34:04 +0530

    allowing cors to use options for preflight

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,9 +6,16 @@ import router from "./routes/route";
 
 dotenv.config();
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL, methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], credentials: true })); //for cross origin requests
-app.options('*', cors());
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: "25mb" })); //allow us to parse incoming requets: req.body
 app.use(express.urlencoded({ extended: true, limit: "25mb" })); //payload size limit increased to 25mb
 app.use(cookieParser()); //allow us to parse incoming cookies
