db1a7e6e1cba9617d045bb563b773095e9e62de7
Utkarsh Jaiswal <89697358+CodeWizard-Ishu@users.noreply.github.com>
2025-06-10 22:02:00 +0530

    Shifting the website into maintenance mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,200 +1,200 @@
-import "./App.css";
-import { useState, lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { inject } from "@vercel/analytics";
-import { injectSpeedInsights } from "@vercel/speed-insights";
+// import "./App.css";
+// import { useState, lazy, Suspense } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { inject } from "@vercel/analytics";
+// import { injectSpeedInsights } from "@vercel/speed-insights";
 
-// Import main/critical components normally
-import Landing from "./pages/Landing";
-import LoginPage from "./pages/Login";
-import SignupPage from "./pages/Signup";
-import ProtectedRoutes from "./utils/ProtectedRoutes";
-import ProtectedBookingRoutes from "./utils/ProtectedBookingRoutes";
-import Spinner from "./components/ui/Spinner";
+// // Import main/critical components normally
+// import Landing from "./pages/Landing";
+// import LoginPage from "./pages/Login";
+// import SignupPage from "./pages/Signup";
+// import ProtectedRoutes from "./utils/ProtectedRoutes";
+// import ProtectedBookingRoutes from "./utils/ProtectedBookingRoutes";
+// import Spinner from "./components/ui/Spinner";
 
-// Lazy load secondary/feature-specific pages
-const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+// // Lazy load secondary/feature-specific pages
+// const ProfilePage = lazy(() => import("./pages/ProfilePage"));
 
-//mentor dashboard pages
-const MentorDashboard = lazy(() => import("./Mentor/MentorDashboard"));
-const MentorHome = lazy(() => import("./Mentor/Home"));
-const MentorMessages = lazy(() => import("./Mentor/Messages"));
-const MentorMeetings = lazy(() => import("./Mentor/Meetings"));
-const MentorServices = lazy(() => import("./Mentor/Services"));
-const MentorTestimonials = lazy(() => import("./Mentor/Testimonials"));
-const MentorCalendar = lazy(() => import("./Mentor/Calender"));
-const MentorAnalytics = lazy(() => import("./Mentor/Analytics"));
-const MentorPayments = lazy(() => import("./Mentor/Payments"));
-const MentorProfileDetails = lazy(() => import("./Mentor/ProfileDetails"));
+// //mentor dashboard pages
+// const MentorDashboard = lazy(() => import("./Mentor/MentorDashboard"));
+// const MentorHome = lazy(() => import("./Mentor/Home"));
+// const MentorMessages = lazy(() => import("./Mentor/Messages"));
+// const MentorMeetings = lazy(() => import("./Mentor/Meetings"));
+// const MentorServices = lazy(() => import("./Mentor/Services"));
+// const MentorTestimonials = lazy(() => import("./Mentor/Testimonials"));
+// const MentorCalendar = lazy(() => import("./Mentor/Calender"));
+// const MentorAnalytics = lazy(() => import("./Mentor/Analytics"));
+// const MentorPayments = lazy(() => import("./Mentor/Payments"));
+// const MentorProfileDetails = lazy(() => import("./Mentor/ProfileDetails"));
 
-//mentee dashboard pages
-const MenteeDashboard = lazy(() => import("./Mentee/MenteeDashboard"));
-const MenteeHome = lazy(() => import("./Mentee/Home"));
-const MenteeMessages = lazy(() => import("./Mentee/Messages"));
-const MenteeMeetings = lazy(() => import("./Mentee/Meetings"));
-const MenteeSettings = lazy(() => import("./Mentee/ProfileSettings"));
+// //mentee dashboard pages
+// const MenteeDashboard = lazy(() => import("./Mentee/MenteeDashboard"));
+// const MenteeHome = lazy(() => import("./Mentee/Home"));
+// const MenteeMessages = lazy(() => import("./Mentee/Messages"));
+// const MenteeMeetings = lazy(() => import("./Mentee/Meetings"));
+// const MenteeSettings = lazy(() => import("./Mentee/ProfileSettings"));
 
-const BookingPage = lazy(() => import("./pages/BookingPage"));
-const CheckAvailability = lazy(() => import("./pages/CheckAvailability"));
-const AboutUs = lazy(() => import("./pages/AboutUs"));
-const SeeAll = lazy(() => import("./pages/SeeAll"));
-const ContactUs = lazy(() => import("./pages/ContactUs"));
-const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
-const Pricing = lazy(() => import("./pages/Pricing"));
-const AllMentors = lazy(() => import("./pages/AllMentors"));
-const ResetPasswordPage = lazy(() => import("./pages/ResetPassword"));
-const BookingSuccessPage = lazy(() => import("./pages/BookingSuccessful"));
-const OnboardingServices = lazy(() => import("./pages/Onboarding/Service"));
-const OnboardingProfile = lazy(() => import("./pages/Onboarding/Profile"));
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
+// const BookingPage = lazy(() => import("./pages/BookingPage"));
+// const CheckAvailability = lazy(() => import("./pages/CheckAvailability"));
+// const AboutUs = lazy(() => import("./pages/AboutUs"));
+// const SeeAll = lazy(() => import("./pages/SeeAll"));
+// const ContactUs = lazy(() => import("./pages/ContactUs"));
+// const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
+// const Pricing = lazy(() => import("./pages/Pricing"));
+// const AllMentors = lazy(() => import("./pages/AllMentors"));
+// const ResetPasswordPage = lazy(() => import("./pages/ResetPassword"));
+// const BookingSuccessPage = lazy(() => import("./pages/BookingSuccessful"));
+// const OnboardingServices = lazy(() => import("./pages/Onboarding/Service"));
+// const OnboardingProfile = lazy(() => import("./pages/Onboarding/Profile"));
+// const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
-// Skeleton loading fallback
-const SkeletonFallback = () => (
-  <Spinner clasName="min-h-screen content-center" />
-);
+// // Skeleton loading fallback
+// const SkeletonFallback = () => (
+//   <Spinner clasName="min-h-screen content-center" />
+// );
 
-const capitalize = (string: string) => {
-  if (!string) return "";
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-};
+// const capitalize = (string: string) => {
+//   if (!string) return "";
+//   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+// };
 
-function App() {
-  injectSpeedInsights();
-  inject();
+// function App() {
+//   injectSpeedInsights();
+//   inject();
 
-  const [loggedIn, setLoggedIn] = useState(() => {
-    return localStorage.getItem("loggedIn") === "true";
-  });
-  const [mentor, setMentor] = useState(() => {
-    return localStorage.getItem("mentor") === "true";
-  });
+//   const [loggedIn, setLoggedIn] = useState(() => {
+//     return localStorage.getItem("loggedIn") === "true";
+//   });
+//   const [mentor, setMentor] = useState(() => {
+//     return localStorage.getItem("mentor") === "true";
+//   });
 
-  const handleLogin = (
-    token: string,
-    isMentor: boolean,
-    isActive: boolean,
-    userId: string,
-    firstName: string,
-    lastName: string,
-    username: string,
-  ) => {
-    localStorage.setItem("userToken", `Bearer ${token}`);
-    localStorage.setItem("loggedIn", "true");
-    localStorage.setItem("mentor", isMentor ? "true" : "false");
-    localStorage.setItem("isActive", isActive ? "true" : "false");
-    localStorage.setItem("userId", userId);
-    localStorage.setItem("fullName", `${capitalize(firstName)} ${capitalize(lastName)}`);
-    localStorage.setItem("username", username);
-    setLoggedIn(true);
-    setMentor(isMentor);
-  };
+//   const handleLogin = (
+//     token: string,
+//     isMentor: boolean,
+//     isActive: boolean,
+//     userId: string,
+//     firstName: string,
+//     lastName: string,
+//     username: string,
+//   ) => {
+//     localStorage.setItem("userToken", `Bearer ${token}`);
+//     localStorage.setItem("loggedIn", "true");
+//     localStorage.setItem("mentor", isMentor ? "true" : "false");
+//     localStorage.setItem("isActive", isActive ? "true" : "false");
+//     localStorage.setItem("userId", userId);
+//     localStorage.setItem("fullName", `${capitalize(firstName)} ${capitalize(lastName)}`);
+//     localStorage.setItem("username", username);
+//     setLoggedIn(true);
+//     setMentor(isMentor);
+//   };
 
-  const handleSignup = (
-    token: string,
-    isMentor: boolean,
-    isActive: boolean,
-    userId: string,
-    firstName: string,
-    lastName: string,
-    username: string,
-  ) => {
-    localStorage.setItem("userToken", `Bearer ${token}`);
-    localStorage.setItem("loggedIn", "true");
-    localStorage.setItem("mentor", isMentor ? "true" : "false");
-    localStorage.setItem("isActive", isActive ? "true" : "false");
-    localStorage.setItem("userId", userId);
-    localStorage.setItem("fullName", `${capitalize(firstName)} ${capitalize(lastName)}`);
-    localStorage.setItem("username", username);
-    setLoggedIn(true);
-    setMentor(isMentor);
-  };
+//   const handleSignup = (
+//     token: string,
+//     isMentor: boolean,
+//     isActive: boolean,
+//     userId: string,
+//     firstName: string,
+//     lastName: string,
+//     username: string,
+//   ) => {
+//     localStorage.setItem("userToken", `Bearer ${token}`);
+//     localStorage.setItem("loggedIn", "true");
+//     localStorage.setItem("mentor", isMentor ? "true" : "false");
+//     localStorage.setItem("isActive", isActive ? "true" : "false");
+//     localStorage.setItem("userId", userId);
+//     localStorage.setItem("fullName", `${capitalize(firstName)} ${capitalize(lastName)}`);
+//     localStorage.setItem("username", username);
+//     setLoggedIn(true);
+//     setMentor(isMentor);
+//   };
 
-  const handleLogout = () => {
-    setLoggedIn(false);
-    setMentor(false);
-  };
+//   const handleLogout = () => {
+//     setLoggedIn(false);
+//     setMentor(false);
+//   };
 
+//   return (
+//     <Router>
+//       <Suspense fallback={<SkeletonFallback />}>
+//         <Routes>
+//           {/* Public Routes */}
+//           <Route path="/" element={<Landing loggedIn={loggedIn} mentor={mentor} />} />
+//           <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+//           <Route path="/signup" element={<SignupPage onSignup={handleSignup} />} />
+//           <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+//           <Route path="/about" element={<AboutUs />} />
+//           <Route path="/contact" element={<ContactUs />} />
+//           <Route path="/privacy" element={<PrivacyPolicy />} />
+//           <Route path="/pricing" element={<Pricing />} />
+//           <Route path="/all-mentors" element={<AllMentors loggedIn={loggedIn} mentor={mentor} />} />
+//           <Route path="/profile/:username" element={<ProfilePage />} />
+//           <Route path="/see-all/:domain" element={<SeeAll loggedIn={loggedIn} mentor={mentor} />} />
+
+//           {/* Protected Routes */}
+//           <Route element={<ProtectedRoutes allowedUserType="mentor" onLogout={handleLogout} />}>
+//             <Route path="/onboarding/services" element={<OnboardingServices />} />
+//             <Route path="/onboarding/profile" element={<OnboardingProfile />} />
+//             <Route path="/dashboard" element={<MentorDashboard onLogout={handleLogout} />} >
+//               <Route index element={<MentorHome />} />
+//               <Route path="messages" element={<MentorMessages />} />
+//               <Route path="meetings" element={<MentorMeetings />} />
+//               <Route path="services" element={<MentorServices />} />
+//               <Route path="testimonials" element={<MentorTestimonials />} />
+//               <Route path="calendar" element={<MentorCalendar />} />
+//               <Route path="analytics" element={<MentorAnalytics />} />
+//               <Route path="payments" element={<MentorPayments />} />
+//               <Route path="settings" element={<MentorProfileDetails />} />
+//             </Route>
+//           </Route>
+
+//           <Route element={<ProtectedRoutes allowedUserType="mentee" onLogout={handleLogout} />}>
+//             <Route path="/dashboard/mentee" element={<MenteeDashboard onLogout={handleLogout} />}>
+//               <Route index element={<MenteeHome />} />
+//               <Route path="messages" element={<MenteeMessages />} />
+//               <Route path="meetings" element={<MenteeMeetings />} />
+//               <Route path="settings" element={<MenteeSettings />} />
+//             </Route>
+//             {/* Booking Flow Routes */}
+//             <Route element={<ProtectedBookingRoutes requireService={true} onLogout={handleLogout} />}>
+//               <Route path="/availability/:username" element={<CheckAvailability />} />
+//             </Route>
+//             <Route element={<ProtectedBookingRoutes requireService={true} requireSlot={true} onLogout={handleLogout} />}>
+//               <Route path="/booking/:username" element={<BookingPage />} />
+//             </Route>
+//             <Route element={<ProtectedBookingRoutes requireService={true} requireSlot={true} onLogout={handleLogout} />}>
+//               <Route path="/booking/successfull" element={<BookingSuccessPage />} />
+//             </Route>
+//           </Route>
+
+//           <Route path="*" element={<NotFoundPage />} />
+//         </Routes>
+//       </Suspense>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import Header from "./components/Header"
+
+const App = () => {
   return (
-    <Router>
-      <Suspense fallback={<SkeletonFallback />}>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Landing loggedIn={loggedIn} mentor={mentor} />} />
-          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
-          <Route path="/signup" element={<SignupPage onSignup={handleSignup} />} />
-          <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/all-mentors" element={<AllMentors loggedIn={loggedIn} mentor={mentor} />} />
-          <Route path="/profile/:username" element={<ProfilePage />} />
-          <Route path="/see-all/:domain" element={<SeeAll loggedIn={loggedIn} mentor={mentor} />} />
-
-          {/* Protected Routes */}
-          <Route element={<ProtectedRoutes allowedUserType="mentor" onLogout={handleLogout} />}>
-            <Route path="/onboarding/services" element={<OnboardingServices />} />
-            <Route path="/onboarding/profile" element={<OnboardingProfile />} />
-            <Route path="/dashboard" element={<MentorDashboard onLogout={handleLogout} />} >
-              <Route index element={<MentorHome />} />
-              <Route path="messages" element={<MentorMessages />} />
-              <Route path="meetings" element={<MentorMeetings />} />
-              <Route path="services" element={<MentorServices />} />
-              <Route path="testimonials" element={<MentorTestimonials />} />
-              <Route path="calendar" element={<MentorCalendar />} />
-              <Route path="analytics" element={<MentorAnalytics />} />
-              <Route path="payments" element={<MentorPayments />} />
-              <Route path="settings" element={<MentorProfileDetails />} />
-            </Route>
-          </Route>
-
-          <Route element={<ProtectedRoutes allowedUserType="mentee" onLogout={handleLogout} />}>
-            <Route path="/dashboard/mentee" element={<MenteeDashboard onLogout={handleLogout} />}>
-              <Route index element={<MenteeHome />} />
-              <Route path="messages" element={<MenteeMessages />} />
-              <Route path="meetings" element={<MenteeMeetings />} />
-              <Route path="settings" element={<MenteeSettings />} />
-            </Route>
-            {/* Booking Flow Routes */}
-            <Route element={<ProtectedBookingRoutes requireService={true} onLogout={handleLogout} />}>
-              <Route path="/availability/:username" element={<CheckAvailability />} />
-            </Route>
-            <Route element={<ProtectedBookingRoutes requireService={true} requireSlot={true} onLogout={handleLogout} />}>
-              <Route path="/booking/:username" element={<BookingPage />} />
-            </Route>
-            <Route element={<ProtectedBookingRoutes requireService={true} requireSlot={true} onLogout={handleLogout} />}>
-              <Route path="/booking/successfull" element={<BookingSuccessPage />} />
-            </Route>
-          </Route>
-
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
-    </Router>
-  );
+    <div className="min-h-screen bg-sky-200">
+      <Header/>
+      <div className="container flex text-center justify-center mt-36 text-2xl bg-white rounded-lg p-4 mx-auto w-3/4"> 
+        Website is currently under maintenance. Please check back later.
+        <br />
+  
+        <br />
+  
+        Sorry for the inconvenience - MentG Team
+      </div>
+    </div>
+  )
 }
 
 export default App;
-
-
-
-
-// import Header from "./components/Header"
-
-// const App = () => {
-//   return (
-//     <div className="min-h-screen bg-sky-200">
-//       <Header/>
-//       <div className="container flex text-center justify-center mt-36 text-2xl bg-white rounded-lg p-4 mx-auto w-3/4"> 
-//       Website is currently under maintenance. Please check back later.
-//       <br />
-
-//       <br />
-
-//       Sorry for the inconvenience - MentG Team
-//     </div>
-//     </div>
-//   )
-// }
-
-// export default App
